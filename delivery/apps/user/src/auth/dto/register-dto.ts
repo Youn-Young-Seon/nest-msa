34108a82c0292a70b4bc580ceb9ba01699963c81
szdbsyyszs <szdbsyyszs32@gmail.com>
2025-05-13 23:51:00 +0900

    user microservice

---
+++ delivery/apps/user/src/auth/dto/register-dto.ts
@@ -0,0 +1,13 @@
+import {IsNotEmpty, IsNumber, IsString} from "class-validator";
+
+export class RegisterDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+    @IsNumber()
+    @IsNotEmpty()
+    age: number;
+    @IsString()
+    @IsNotEmpty()
+    profile: string;
+}
